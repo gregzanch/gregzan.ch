5b8d44697581ba49f7176323f760deec430f4c5a
Greg Zanchelli <zanchelli.greg@gmail.com>
2021-03-26 01:16:51 -0400

    meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Colors, GlobalStyle } from "components/StyleGuide";
 import { ThemeProvider } from "styled-components";
 import "./index.css";
@@ -18,6 +19,33 @@ type Props = {
 function App({ Component, pageProps }: Props) {
   return (
     <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/site.webmanifest"/>
+        <meta name="msapplication-config" content="/browserconfig.xml" />
+        <meta name="msapplication-TileColor" content="#ededed"/>
+        <meta name="theme-color" content="#ededed"/>
+
+        <title>gregzan.ch</title>
+        <meta name="title" content="gregzan.ch"/>
+        <meta name="description" content="I'm a mechanical, acoustical, and software engineer. My research interests revolve around scientific computing, primarily computational acoustics."/>
+
+
+        <meta property="og:type" content="website"/>
+        <meta property="og:url" content="https://gregzan.ch/"/>
+        <meta property="og:title" content="gregzan.ch"/>
+        <meta property="og:description" content="I'm a mechanical, acoustical, and software engineer. My research interests revolve around scientific computing, primarily computational acoustics."/>
+        <meta property="og:image" content="/cram3.png"/>
+
+
+        <meta property="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:url" content="https://gregzan.ch/"/>
+        <meta property="twitter:title" content="gregzan.ch"/>
+        <meta property="twitter:description" content="I'm a mechanical, acoustical, and software engineer. My research interests revolve around scientific computing, primarily computational acoustics."/>
+        <meta property="twitter:image" content="/cram3.png"/>
+      </Head>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
         <Component {...pageProps} />
